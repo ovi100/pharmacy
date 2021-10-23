4d3cfc2666f9088c7e768bb04a8a081b6f1eaf68
ovi100 <ovi0167@hotmail.com>
2021-10-23 21:26:52 +0600

    Home page collection slider navigation issue fixed

--- js/scripts.js
+++ js/scripts.js
@@ -128,35 +128,26 @@ $(document).ready(function () {
   });
 
   // Collection Slider/Slick
-  let collectionSlider = $('.collection-slider');
-
-  $('.prev').click(function () {
-    collectionSlider.slick('slickPrev');
-    return false;
-  });
-
-  $('.next').click(function () {
-    collectionSlider.slick('slickNext');
-    return false;
-  });
-
-  collectionSlider.slick({
-    arrows: false,
+  
+  $('.collection-slider').slick({
     infinite: false,
     slidesToShow: 6,
     slidesToScroll: 6,
     speed: 800,
+    nextArrow: '<button class="next"><svg height="30px" viewBox="0 0 1792 1792" width="30px"><path d="M1171 960q0 13-10 23l-466 466q-10 10-23 10t-23-10l-50-50q-10-10-10-23t10-23l393-393-393-393q-10-10-10-23t10-23l50-50q10-10 23-10t23 10l466 466q10 10 10 23z"/></svg></button>',
+    prevArrow: '<button class="prev"><svg height="30px" viewBox="0 0 1792 1792" width="30px"><path d="M1203 544q0 13-10 23l-393 393 393 393q10 10 10 23t-10 23l-50 50q-10 10-23 10t-23-10l-466-466q-10-10-10-23t10-23l466-466q10-10 23-10t23 10l50 50q10 10 10 23z"/></svg></button>',
     responsive: [
       {
         breakpoint: 1366,
         settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4
+          slidesToShow: 5,
+          slidesToScroll: 5
         }
       },
       {
         breakpoint: 768,
         settings: {
+          arrows: false,
           slidesToShow: 2,
           slidesToScroll: 2
         }
